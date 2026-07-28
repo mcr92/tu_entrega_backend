da91be4d29319182d379db8bbd2a0753daf45f0d
Manuel Cruz <manuelcruzrodirguez92@gmail.com>
2026-07-28 14:30:01 -0400

    agregar engine a la configuracion

--- tu_entrega_api/settings.py
+++ tu_entrega_api/settings.py
@@ -98,14 +98,12 @@ DATABASES = {
 
 if PRODUCTION:
     DATABASES = {
-        'default': {
-            'default': dj_database_url.config(
+        'default': dj_database_url.config(
                 default=os.environ.get('DATABASE_URL'),
                 conn_max_age=600,
                 ssl_require=True,
                 engine='django.db.backends.postgresql'
             )
-        }
     }
 
 MIGRATION_MODULES = {
